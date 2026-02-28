06f7549337522270bf795fc71233e396496c1302
Mr Nichosik <asyamovilya@gmail.com>
2026-02-28 17:46:38 +0700

    index complete

--- src/projects/EniseiGardens/js/index.js
+++ src/projects/EniseiGardens/js/index.js
@@ -1,12 +1,40 @@
 
-import { Burger } from "./burger"; 
+import { Burger } from "./burger";
 
 class Index
 {
+    Observer;
+    HiddenItems;
+
     constructor()
     {
         document.documentElement.classList.add("js-enabled");
         new Burger();
+
+        this.Observer = new IntersectionObserver((entries, observer) => this.Observe(entries, observer))
+        this.HiddenItems = document.querySelectorAll(".hidden");
+        this.HandleObvservation(this.HiddenItems);
+    }
+
+    Observe(entries, observer)
+    {
+        entries.forEach(entry => 
+        {
+            if (entry.isIntersecting)
+            {
+                const element = entry.target;
+                element.classList.add("show");
+                observer.unobserve(element);
+            }
+        });
+    }
+
+    HandleObvservation(items)
+    {
+        items.forEach(item =>
+        {
+            this.Observer.observe(item);
+        });
     }
 }
 
