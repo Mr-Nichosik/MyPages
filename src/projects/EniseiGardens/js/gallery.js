3522d6623bb33904ec1cd95803f6b5f68fe49603
Mr Nichosik <asyamovilya@gmail.com>
2026-03-10 16:13:53 +0700

    job complete

--- src/projects/EniseiGardens/js/gallery.js
+++ src/projects/EniseiGardens/js/gallery.js
@@ -3,10 +3,35 @@ import { Burger } from "./burger";
 
 class Gallery
 {
-        constructor()
+    constructor()
     {
         document.documentElement.classList.add("js-enabled");
         new Burger();
+
+        this.Observer = new IntersectionObserver((entries, observer) => this.Observe(entries, observer))
+        this.HiddenItems = document.querySelectorAll(".hidden");
+        this.HandleObvservation(this.HiddenItems);
+    }
+
+    Observe(entries, observer)
+    {
+        entries.forEach(entry => 
+        {
+            if (entry.isIntersecting)
+            {
+                const element = entry.target;
+                element.classList.add("show");
+                observer.unobserve(element);
+            }
+        });
+    }
+
+    HandleObvservation(items)
+    {
+        items.forEach(item =>
+        {
+            this.Observer.observe(item);
+        });
     }
 }
 
